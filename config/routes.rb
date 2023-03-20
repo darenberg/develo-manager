cb7d7616852caa5f29291b7c7715b33b1a9bd1dc
alienor darenberg <alienordarenberg@gmail.com>
2023-03-20 17:22:49 -0600

    remade routes made about it

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :projects
-  resources :dots, only: [:create, :update, :destroy]
-  resources :rooms, only: [:create, :update, :destroy]
-  resources :notes, only: [:create, :update, :destroy]
-  resources :project_members, only: [:create, :update, :destroy]
+  resources :projects do
+    resources :rooms, only: [:create, :update]
+    resources :project_members, only: [:create, :update]
+  end
+
+  resources :rooms, only: [:destroy] do
+    resources :dots, only: [:create, :update]
+  end
+
+  resources :dots, only: :destroy do
+    resources :notes, only: [:create, :update]
+  end
+
   resources :project_admins, only: [:create, :update, :destroy]
+  resources :project_members, only: :destroy
+  resources :dots, only: :destroy
+  resources :notes, only: :destroy
 end
