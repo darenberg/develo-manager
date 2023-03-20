89a13702578583c9841d883b808d9b21dd76bb55
alienor darenberg <alienordarenberg@gmail.com>
2023-03-20 16:21:42 -0600

    feature/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :projects
+  resources :dots, only: [:create, :update, :destroy]
+  resources :rooms, only: [:create, :update, :destroy]
+  resources :notes, only: [:create, :update, :destroy]
+  resources :project_members, only: [:create, :update, :destroy]
+  resources :project_admins, only: [:create, :update, :destroy]
 end
