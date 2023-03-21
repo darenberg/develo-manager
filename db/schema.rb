17b5dbb5fc219a35a4a3ffc9edbddfc2e0e2cfab
alienor darenberg <alienordarenberg@gmail.com>
2023-03-21 12:19:07 -0600

    fixed models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_180537) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_181843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_180537) do
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "fieldname"
     t.index ["project_id"], name: "index_floors_on_project_id"
   end
 
