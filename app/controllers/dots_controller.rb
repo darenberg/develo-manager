833244338d9b833aa46b9e25d7313d954bcadd02
david betech <betech35@gmail.com>
2023-03-22 14:46:34 -0600

    dot for pull

--- app/controllers/dots_controller.rb
+++ app/controllers/dots_controller.rb
@@ -1,3 +1,16 @@
 class DotsController < ApplicationController
+  def create
+    @dot = Dot.new
+    @plan = plan.find(params[:plan_id])
+    @dot.plan = @plan
+
+
+    if @dot.save!
+      redirect_to @plan.floor.project
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 
 end
