d9bda8c8208fb7a5e014c24a556ca8e4b08f4b55
david betech <betech35@gmail.com>
2023-03-28 10:57:23 -0600

    a bunch of things added

---
+++ app/javascript/controllers/task_filter_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="task-filter"
+export default class extends Controller {
+  static values = {
+    projectId: String
+  }
+
+  connect() {
+  }
+
+  searchTags(e) {
+    e.preventDefault()
+    console.log(this.projectIdValue);
+    console.log(e.target.querySelector("input").value)
+    const url = `${e.target.action}?tag_name=${e.target.querySelector("input").value}`
+    fetch(url, { headers: {"Accept": "text/plain"} })
+      .then(response => response.text())
+      .then((data) => {
+        console.log(data)
+      })
+  }
+}
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,7 @@ class Task < ApplicationRecord
   belongs_to :dot
   has_many :user_tasks, dependent: :destroy
   has_one_attached :photo
-
+  has_many :tags,dependent: :destroy
   validates :title, presence: true
   validates :content, presence: true
-  validates :tags, presence: true
 end
